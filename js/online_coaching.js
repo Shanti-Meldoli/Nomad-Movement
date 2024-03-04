dd674260292d46f3e91cc40ab02efaefa681ebe1
Shanti-Meldoli <s.meldoli98@gmail.com>
2024-03-04 17:21:18 +0100

    Web updates
    Added toggle FAQs
    Corrected the arrow padding on testimonials
    Added Colleen’s testimonial

--- js/online_coaching.js
+++ js/online_coaching.js
@@ -1,7 +1,9 @@
 'use strict'
 
-// When i CLICK on .burger, to .header__links TOGGLE the class .active & to body TOGGLE the class .no-scrollbar
-// When i CLICK on .header__a, to .header__links REMOVE the class .active
+// --- Burguer Menu ---
+
+// When CLICK on .burger, to .header__links TOGGLE the class .active & to body TOGGLE the class .no-scrollbar
+// When CLICK on .header__a, to .header__links REMOVE the class .active
 
 const burger = document.querySelector('.burger')
 const headerLinks = document.querySelector('.header__links')
@@ -17,3 +19,46 @@ headerA.forEach(index => {
     headerLinks.classList.remove('active')
   })
 })
+// --- Burguer Menu ---
+
+// --- FAQS ---
+
+// When CLICK on .faqs__question, close all faqs__answer and remove .active from SVGs, then open faqs__answer and rotate the SVG by adding .active class to SVG
+// If it is already open, close it and remove the .active class from the SVG
+
+const faqsA = document.querySelectorAll('.faqs__answer')
+const heightA = []
+const faqsQ = document.querySelectorAll('.faqs__question')
+const SVGs = document.querySelectorAll('.faqs__svg')
+
+// Calculate and store the natural height for each answer, then set their height to 0
+faqsA.forEach((eachA, index)=>{
+  heightA.push(faqsA[index].offsetHeight) // Store the natural height
+  faqsA[index].style.height = 0 // Initially hide the answer
+})
+
+// Create the accordeon effect
+faqsQ.forEach ((eachQ, index)=>{
+  faqsQ[index].addEventListener('click', ()=>{
+    // Determine if the clicked question's answer is currently open
+    const isOpen = faqsA[index].style.height !== '0px';
+    // First, close all answers
+    faqsA.forEach((eachA, index)=>{
+      faqsA[index].style.height = 0
+    })
+    // If the answer was not already open (meaning it's closed), open it by setting its height
+    if (!isOpen) {
+      faqsA[index].style.height = heightA[index] + 'px';
+    }
+    // If it was open, it will be closed as its height was set to '0px' in the loop above
+    
+    // Finally, REMOVE the active class from all SVGs and rotate the SVG of the same index as the clicked question, also if clicked again on the quesiton, remove the active class from the SVG
+    SVGs.forEach((eachSVG, index)=>{
+      SVGs[index].classList.remove('active')
+    })
+    if (!isOpen) {
+      SVGs[index].classList.add('active')
+    }
+  })
+})
+// --- FAQS ---
